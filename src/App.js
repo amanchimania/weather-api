25f00cfc77a57061017f0c298eaf3d2a1d649e31
amanchimania <amanchimania@gmail.com>
2021-03-22 21:21:39 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [cityWeather, setCityWeather] = useState({});
   const [flag,setFlag]=useState(false)
   const fetchCityWeather = () => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c496af33d732297c30b641b3b235e47`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c496af33d732297c30b641b3b235e47`)
       .then((response) => response.json())
       .then((result) => {
         setCityWeather(result)
